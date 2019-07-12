3a851402099f4eb48ff0d7ba2bdc66f6c1955c7b
jackeybiao <hanbiao4079@qq.com>
2019-07-12 17:43:13 +0800

    修改样式

--- src/app/core/post-detail/post-detail.component.ts
+++ src/app/core/post-detail/post-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { JapeCoreService, Article } from '../jape-core.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -12,12 +13,15 @@ import marked from '../../config/marked';
 export class PostDetailComponent implements OnInit {
 
   article: Article;
+  label: Array<string>;
+  locationUrl: string;
 
   content = '';
 
   constructor(
     private japeCore: JapeCoreService,
     private route: ActivatedRoute,
+    public location: Location
     ) { }
 
   ngOnInit() {
@@ -25,10 +29,20 @@ export class PostDetailComponent implements OnInit {
   }
 
   getPost() {
+    const regs = /\<h3([\S\s]*?)\>([\S\s\t]*?)\<\/h3\>|\<h4([\S\s]*?)\>([\S\s\t]*?)\<\/h4\>/igm;
     const sha = this.route.snapshot.paramMap.get('sha');
+    this.locationUrl = this.location.path();
     this.japeCore.getPostDetail(sha)
       .subscribe(article => {
         this.content = marked(decodeURIComponent(escape(window.atob(article.content))));
+        const list = this.content.match(regs);
+        const label = [];
+        if (list) {
+          list.forEach(ele => {
+            label.push(ele.match(/\"([\S\s]*?)\"/ig)[0].replace(/"/, ''));
+          });
+        }
+        this.label = label;
       });
   }
 }
